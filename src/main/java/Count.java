a21fc1e5baa42aae507e2e1de6b9da7588d91cb3
Ryan-Houpt <rehoupt@gmail.com>
2018-12-07 15:18:50 -0600

    servlet exercise

--- src/main/java/Count.java
+++ src/main/java/Count.java
@@ -3,14 +3,21 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
-public class HelloWorldServlet extends HttpServlet {
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/count")
+public class Count extends HttpServlet {
+
+    public static int count = 1;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().println("You should see this in the browser");
-        String name = request.getParameter("name");
-        response.getWriter().write("Hello " + name);
+
+        response.getWriter().print("You have viewed the page " + count + " many times.");
+        count++;
+        String name = request.getParameter("reset");
+        if (name.equals("yes")) {
+            count = 0;
+        }
+
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
